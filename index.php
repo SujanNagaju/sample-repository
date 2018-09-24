b641a48137ae5a01de934d7d013da471d0fc11e5
Sujan Nagaju <nagaju.sujan@webexpertsnepal.com>
2018-09-24 17:48:35 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index</title>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<?php echo "This is a index file."; ?>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Git test</title>
+</head>
+<body>
+	<div class="messages">
+		<?php echo "This is a sample file for git test"; ?><br/>
+		<?php echo "The first change in the file"; ?>
+	</div>
+
+</body>
+</html>
